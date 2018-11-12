55b4918975a041701ec0f49f28441c964cb3b662
Erica Skerrett <eskerrett11@gmail.com>
2018-11-12 15:43:38 -0500

    adds new_patient function

--- client.py
+++ client.py
@@ -5,13 +5,12 @@ inputs = {
     "attending_email": "suyash.kumar@duke.edu",
     "user_age": 50,
 }
+r = requests.post("http://127.0.0.1:5001/new_patient", json=inputs) #how to put /api in url
 
-r = requests.post("http://127.0.0.1:5000/api/new_patient", json = inputs)
 
 inputs2 = {
     "patient_id": "1",
     "heart_rate": 100,
 }
-
-r = requests.post("http://127.0.0.1:5000/api/heart_rate", json = inputs2)
+r = requests.post("http://127.0.0.1:5000/heart_rate", json = inputs2)
 
