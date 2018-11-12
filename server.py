7dc83167470d155dc101d212e7c1e8c80157c9f0
Erica Skerrett <eskerrett11@gmail.com>
2018-11-12 15:47:53 -0500

    adds new_patient function

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+
 @app.route("/api/new_patient", methods=["POST"])
 def new_patient():
     # initialize patient and accept future HR measurements
@@ -15,7 +16,7 @@ def new_patient():
 """
 @app.route("/heart_rate", methods=["POST"])
 def heart_rate_store(p_id, p_hr):
-    # store HR measurement for user with that email and include current time stamp
+    # store HR measurement for user with that email; include current time stamp
     r = request.get_json()
     if r.get("patient_id") == p_id:
         p_hr.append(r.get("heart_rate"))
@@ -35,10 +36,8 @@ def status(patient_id, p_id, p_hr, p_age):
 
 
 def is_tachycardic(p_hr, p_age):
-    
     return status
-    
-    
+
 
 @app.route("/api/heart_rate/<patient_id>", methods=["GET"])
 def heart_rate(patient_id):
@@ -64,5 +63,5 @@ def interval_average():
     return jsonify(output)
 """
 
-if __name__== "__main__":
-    app.run(host="127.0.0.1", port = 5001)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5001)
